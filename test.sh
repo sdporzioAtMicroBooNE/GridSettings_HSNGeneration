3b8f8cd5354c2d2d9d3027ca97f51139807fa439
sdporzio <salvatore.porzio@postgrad.manchester.ac.uk>
2017-10-13 09:08:42 -0500

    Fixing sample.fcl to ignore PMT database

---
+++ test.sh
@@ -0,0 +1,3 @@
+project.py --xml Utilities/OutputXml/gridSettings_channel1_mass0.150_n50000.xml --stage sim --check
+project.py --xml Utilities/OutputXml/gridSettings_channel1_mass0.300_n50000.xml --stage sim --check
+project.py --xml Utilities/OutputXml/gridSettings_channel1_mass0.450_n50000.xml --stage sim --check
